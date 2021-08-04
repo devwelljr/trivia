372b7d5953dc0add16f1565f1509da6fc957cbec
Wellington <junior.wel02@exemplo.br>
2021-08-04 15:41:36 -0300

    Requisito 12

---
+++ src/pages/Feedback.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import GameHeader from '../components/GameHeader';
+
+class Feedback extends Component {
+  render() {
+    return (
+      <div>
+        <GameHeader />
+      </div>
+    );
+  }
+}
+
+export default Feedback;
