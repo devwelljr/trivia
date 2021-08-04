ce3f09797d927add7ae86360fe48fccbcb1773a4
André Moreno <deco616@hotmail.com>
2021-08-04 18:55:49 -0300

    Req15 e req18 - Botao "Jogar novamente" para o /

--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -1,14 +1,22 @@
 import React, { Component } from 'react';
+import { string, func, number, object, objectOf, oneOfType } from 'prop-types';
 import GameHeader from '../components/GameHeader';
+import ButtonToLogin from '../components/ButtonToLogin';
 
 class Feedback extends Component {
   render() {
+    const { history } = this.props;
     return (
       <div>
         <GameHeader />
+        <ButtonToLogin history={ history } dataTestId="btn-play-again" />
       </div>
     );
   }
 }
+
+Feedback.propTypes = {
+  history: objectOf(oneOfType([func, string, number, object])).isRequired,
+};
 
 export default Feedback;
