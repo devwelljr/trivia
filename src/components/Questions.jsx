f76c6ddf752594d14e421f9d517b0ba307c0e656
Andre Jaques <andrejaques.b@gmail.com>
2021-08-05 15:26:38 -0300

    req07 - adiciona logica do style da resposta

--- src/components/Questions.jsx
+++ src/components/Questions.jsx
@@ -6,12 +6,16 @@ import { fetchAPIQuestion } from '../redux/actions';
 class Questions extends Component {
   constructor() {
     super();
+
     this.state = {
       // questions: [],
       idQuestion: 0,
+      isAnswered: false,
     };
+
     this.findQuestion = this.findQuestion.bind(this);
     this.renderQuestion = this.renderQuestion.bind(this);
+    this.handleClick = this.handleClick.bind(this);
   }
 
   componentDidMount() {
@@ -28,17 +32,20 @@ class Questions extends Component {
     return response;
   }
 
+  handleClick() {
+    this.setState({
+      isAnswered: true,
+    });
+  }
+
   renderQuestion() {
-    const { idQuestion } = this.state;
-    const { questions } = this.props;
-    const { results } = questions;
-    const {
-      category,
-      question,
-      correct_answer: correctAnswer,
+    const { idQuestion, isAnswered } = this.state;
+    const { questions: { results } } = this.props;
+    const styleWrong = { border: '3px solid rgb(255, 0, 0)' };
+    const styleCorrect = { border: '3px solid rgb(6, 240, 15)' };
+    const { category, question, correct_answer: correctAnswer,
       incorrect_answers: incorrectAnswer,
-      type,
-    } = results[idQuestion];
+      type } = results[idQuestion];
     return (
       <div>
         <h4 data-testid="question-category">{ category }</h4>
@@ -49,34 +56,59 @@ class Questions extends Component {
               <button
                 data-testid="wrong-answer-0"
                 type="button"
+                disabled={ isAnswered }
+                style={ isAnswered ? styleWrong : null }
+                onClick={ this.handleClick }
               >
                 { incorrectAnswer }
               </button>)
-
             : incorrectAnswer.map((answer, index) => (
               <button
                 data-testid={ `wrong-answer-${index}` }
                 key={ index }
                 type="button"
                 value={ answer }
+                disabled={ isAnswered }
+                style={ isAnswered ? styleWrong : null }
+                onClick={ this.handleClick }
               >
                 { answer }
               </button>
             ))
         }
-        <button data-testid="correct-answer" type="button">{ correctAnswer }</button>
+        <button
+          data-testid="correct-answer"
+          type="button"
+          disabled={ isAnswered }
+          style={ isAnswered ? styleCorrect : null }
+          onClick={ this.handleClick }
+        >
+          { correctAnswer }
+        </button>
       </div>
     );
   }
 
   render() {
+    const { isAnswered } = this.state;
     const { questions } = this.props;
     const { results } = questions;
     return (
       <div>
-        {
-          results ? this.renderQuestion() : null
-        }
+        <div className="questions">
+          {
+            results ? this.renderQuestion() : null
+          }
+        </div>
+        <div className="timer" />
+        <button
+          type="button"
+          className="button-next"
+          data-testid="btn-next"
+          disabled={ !isAnswered }
+        >
+          Pŕoxima
+        </button>
       </div>
     );
   }
