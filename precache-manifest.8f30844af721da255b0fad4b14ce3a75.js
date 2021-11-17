7f4f4fa256647b028377ab1b2f43e139bdd11684
Wellington <junior.wel02@exemplo.br>
2021-11-17 16:02:03 -0300

    Updates

---
+++ precache-manifest.8f30844af721da255b0fad4b14ce3a75.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25b5655c800bb2f602af0cd7337da8e4",
+    "url": "/trivia/index.html"
+  },
+  {
+    "revision": "7b81ceed37b151210223",
+    "url": "/trivia/static/css/2.e78a7c17.chunk.css"
+  },
+  {
+    "revision": "690311588393f6c5a1b1",
+    "url": "/trivia/static/css/main.d483f28d.chunk.css"
+  },
+  {
+    "revision": "7b81ceed37b151210223",
+    "url": "/trivia/static/js/2.dc5567b0.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/trivia/static/js/2.dc5567b0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "690311588393f6c5a1b1",
+    "url": "/trivia/static/js/main.18777646.chunk.js"
+  },
+  {
+    "revision": "23dbb679c736924e4b1a",
+    "url": "/trivia/static/js/runtime-main.6e763e45.js"
+  }
+]);
